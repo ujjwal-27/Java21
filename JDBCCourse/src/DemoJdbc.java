049be53e1c4cd296c949975629e7b0887c14ae20
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-12 14:01:22 +0545

    create connection

--- JDBCCourse/src/DemoJdbc.java
+++ JDBCCourse/src/DemoJdbc.java
@@ -11,9 +11,16 @@ public class DemoJdbc {
          * Process the result
          * Close connection
          */
-        
+
+        // Here, java will first connect with jdbc. Then, jdbc will connect with the dbms i.e. postgresql. Finally, we'll have to mention the db name which is in the dbms.
+        String url = "jdbc:postgresql://localhost:5432/demo";
+        String uname = "postgres";
+        String pass = "postgres";
+
         // Loading and registering driver. This step is optional. It throws ClassNotFoundException which must be handled through try/catch. Since this is just demo, we'll duck exception.
         Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection(url, uname, pass); // creating connection
+        System.out.println("connection established");
 
     }
 }
