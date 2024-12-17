8ffbd44fbc7c295661f2446c7fb432f7e7d1043d
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-17 16:55:59 +0545

    print hello world

--- SpringBootDemo/src/main/java/com/ujjwal/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/ujjwal/SpringBootDemo/SpringBootDemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
