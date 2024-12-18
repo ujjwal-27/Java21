4c44a5dbe039e54e867d239956476592f547f344
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-18 17:27:14 +0545

    declaring class as component using annotation

--- SpringBootDemo/src/main/java/com/ujjwal/SpringBootDemo/Programmer.java
+++ SpringBootDemo/src/main/java/com/ujjwal/SpringBootDemo/Programmer.java
@@ -1,5 +1,8 @@
 package com.ujjwal.SpringBootDemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Programmer {
     public void code() {
         System.out.println("Coding...");
