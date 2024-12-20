d645631312940472b956c81dd0f5d9e53d76499a
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-20 16:15:05 +0545

    autowire cpu bean

--- SpringBootDemo/src/main/java/com/ujjwal/SpringBootDemo/Laptop.java
+++ SpringBootDemo/src/main/java/com/ujjwal/SpringBootDemo/Laptop.java
@@ -1,11 +1,16 @@
 package com.ujjwal.SpringBootDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Laptop {
+    @Autowired
+    Cpu cpu;
 
     public void compile() {
         System.out.println("Compiling...");
+
+        cpu.processor();
     }
 }
