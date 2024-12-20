5a2bb4d0bd396283dbd7c2cf4e762dccedebac18
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-20 16:13:09 +0545

    create Laptop bean

---
+++ SpringBootDemo/src/main/java/com/ujjwal/SpringBootDemo/Laptop.java
@@ -0,0 +1,11 @@
+package com.ujjwal.SpringBootDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile() {
+        System.out.println("Compiling...");
+    }
+}
