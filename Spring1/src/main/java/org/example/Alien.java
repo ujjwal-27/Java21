2d103c710512bf83e205286f8d8dc86a384e9397
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-25 23:17:25 +0545

    create getter and setter for laptop

--- Spring1/src/main/java/org/example/Alien.java
+++ Spring1/src/main/java/org/example/Alien.java
@@ -1,15 +1,25 @@
 package org.example;
 
 public class Alien {
-    private int age; 
+    private int age;
     private Laptop lap;
 
     public Alien() {
         System.out.println("This is alien");
     }
 
+    public void code() {
+        System.out.println("Coding");
+
+        lap.compile();
+    }
+
     public int getAge() {
         return age;
+    }
+
+    public Laptop getLap() {
+        return lap;
     }
 
     public void setAge(int age) {
@@ -17,9 +27,7 @@ public class Alien {
         this.age = age;
     }
 
-    public void code() {
-        System.out.println("Coding");
-
-        lap.compile();
+    public void setLap(Laptop lap) {
+        this.lap = lap;
     }
 }
