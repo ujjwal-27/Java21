f06eddf4d6e693ccda452a64a1158e8c09ca4c43
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-25 19:02:09 +0545

    testing prototype scope

--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -11,14 +11,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Creates container, and helps to get object from the container
+        // Creates 'container' in spring
+        // When the scope is 'singleton' on a bean tag, the object is also created.
+        // But, when the scope is 'prototype' on a bean tag, only the container is created.
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        // By default, getBean provides an object. Here, we're typecasting the object to Alien.
-        Alien alien = (Alien) context.getBean("alien");
-        alien.code();
+        // By default, getBean() returns an object. Here, we're typecasting Alien as reference.
+        // Since the 'scope' of Alien bean is 'prototype', it creates two different objects as it is being called twice.
+        // If the scope is 'singleton', both the reference variable i.e. alien2 and alien2 would refer to same object.
+        Alien alien1 = (Alien) context.getBean("alien");
+        alien1.age = 27; // if the scope was singleton, this change would be displayed for both objects created i.e. alien1 and alien2
+        System.out.println(alien1.age);
 
         Alien alien2 = (Alien) context.getBean("alien");
-        alien2.code();
+        System.out.println(alien2.age);
     }
 }
