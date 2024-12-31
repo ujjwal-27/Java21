6f15268463b95a5756be12a7c11d8706d5b8251e
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-31 18:58:13 +0545

    minor updates

--- Spring1/src/main/java/org/example/xml/App.java
+++ Spring1/src/main/java/org/example/xml/App.java
@@ -24,6 +24,6 @@ public class App
         System.out.println(alien.getSalary());
         alien.code();
 
-        Laptop laptop = (Laptop) context.getBean("computer");
+//        Laptop laptop = (Laptop) context.getBean("computer"); // This bean will not be accessible here because it is being used as an inner bean for Alien class.
     }
 }
