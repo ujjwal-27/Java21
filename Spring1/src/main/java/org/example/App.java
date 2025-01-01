ebbb9af3679799403dc3596b19133129aaea5958
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-01 16:12:27 +0545

    invoke Desktop bean

--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -7,5 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class); // creates and initializes Spring IoC container
+
+        Desktop desktop = context.getBean(Desktop.class);
     }
 }
