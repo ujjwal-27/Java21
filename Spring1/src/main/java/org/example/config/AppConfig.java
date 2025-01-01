a7fd48810f2183b86bc03b0d0f5985dca0a0f91b
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-01 16:12:10 +0545

    create Desktop bean

--- Spring1/src/main/java/org/example/config/AppConfig.java
+++ Spring1/src/main/java/org/example/config/AppConfig.java
@@ -1,8 +1,13 @@
 package org.example.config;
 
+import org.example.Desktop;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
+    @Bean
+    public Desktop desktop() {
+        return new Desktop();
+    }
 }
