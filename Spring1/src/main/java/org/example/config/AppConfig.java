2e9a3318917992bc6412d56e983351430195f336
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-01 16:17:31 +0545

    add comments

--- Spring1/src/main/java/org/example/config/AppConfig.java
+++ Spring1/src/main/java/org/example/config/AppConfig.java
@@ -4,6 +4,10 @@ import org.example.Desktop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @Configuration If we want to configure our application using 'java based configuration', the 'config class' must be wrapped with this annotation.
+ * @Bean If we want to create a bean, or declare a class as a bean, this annotation is used.
+ */
 @Configuration
 public class AppConfig {
     @Bean
