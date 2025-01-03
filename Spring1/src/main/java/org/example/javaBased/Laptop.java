46f7b1219279bba8b599191426d8805f42d2a1f5
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-03 18:44:44 +0545

    create laptop class implementing computer

---
+++ Spring1/src/main/java/org/example/javaBased/Laptop.java
@@ -0,0 +1,13 @@
+package org.example.javaBased;
+
+public class Laptop implements Computer {
+    public Laptop() {
+        System.out.println("This is laptop");
+    }
+
+    @Override
+    public void compile() {
+        System.out.println("Compiling from laptop");
+    }
+
+}
