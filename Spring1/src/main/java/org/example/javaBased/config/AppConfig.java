7f5c5ff230f513b41637d56103443976f7f94af9
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-03 22:34:58 +0545

    use ComponentScan annotation in container

--- Spring1/src/main/java/org/example/javaBased/config/AppConfig.java
+++ Spring1/src/main/java/org/example/javaBased/config/AppConfig.java
@@ -11,8 +11,10 @@ import org.springframework.context.annotation.*;
 /**
  * @Configuration If we want to configure our application using 'java based configuration', the 'config class' must be wrapped with this annotation.
  * @Bean If we want to create a bean, or declare a class as a bean, this annotation is used.
+ * @ComponentScan This annotation takes path of package as parameter where classes are registered as component.
  */
 @Configuration
+@ComponentScan("org.example.javaBased")
 public class AppConfig {
 //    /**
 //     * -- Autowire:
