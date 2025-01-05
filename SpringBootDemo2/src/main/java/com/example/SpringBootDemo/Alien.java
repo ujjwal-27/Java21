cea5fbf0a1bc295bf00ff1cb88ae44c64523c5b9
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 23:35:46 +0545

    create instance variables, getters and setters

--- SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Alien.java
+++ SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Alien.java
@@ -1,5 +1,24 @@
 package com.example.SpringBootDemo;
 
 public class Alien {
-    
+    private int age;
+    private Computer computer;
+
+    public int getAge() {
+        return age;
+    }
+
+    public Computer getComputer() {
+        return computer;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setComputer(Computer computer) {
+        this.computer = computer;
+    }
+
+
 }
