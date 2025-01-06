ae36d09b8333b6ef54ce326a9401fa3df1c13a57
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-06 16:29:46 +0545

    create constructors: Laptop and Desktop

--- SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Desktop.java
+++ SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Desktop.java
@@ -1,6 +1,10 @@
 package com.example.SpringBootDemo;
 
 public class Desktop implements Computer {
+    public Desktop() {
+        System.out.println("Desktop object created");
+    }
+
     public void compile() {
         System.out.println("Compiling from Desktop...");
     }
--- SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Laptop.java
+++ SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Laptop.java
@@ -1,6 +1,10 @@
 package com.example.SpringBootDemo;
 
 public class Laptop implements Computer {
+    public Laptop() {
+        System.out.println("Laptop object created");
+    }
+
     public void compile() {
         System.out.println("Compiling from Laptop...");
     }
