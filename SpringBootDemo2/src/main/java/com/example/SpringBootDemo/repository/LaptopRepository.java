10d064ce5927f36c88acad58b5f808f6096fed6e
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-06 22:30:43 +0545

    implement Repository annotation

--- SpringBootDemo2/src/main/java/com/example/SpringBootDemo/repository/LaptopRepository.java
+++ SpringBootDemo2/src/main/java/com/example/SpringBootDemo/repository/LaptopRepository.java
@@ -1,7 +1,9 @@
 package com.example.SpringBootDemo.repository;
 
 import com.example.SpringBootDemo.model.Laptop;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class LaptopRepository {
     public void save(Laptop laptop) {
         System.out.println("Saved in database");
