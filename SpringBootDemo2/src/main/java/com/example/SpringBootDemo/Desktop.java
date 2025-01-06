b01aa9806a98ee42da82401787b160a58f79e1a6
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-06 16:34:09 +0545

    remove constructors

--- SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Desktop.java
+++ SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Desktop.java
@@ -1,10 +1,6 @@
 package com.example.SpringBootDemo;
 
 public class Desktop implements Computer {
-    public Desktop() {
-        System.out.println("Desktop object created");
-    }
-
     public void compile() {
         System.out.println("Compiling from Desktop...");
     }
--- SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Laptop.java
+++ SpringBootDemo2/src/main/java/com/example/SpringBootDemo/Laptop.java
@@ -1,10 +1,6 @@
 package com.example.SpringBootDemo;
 
 public class Laptop implements Computer {
-    public Laptop() {
-        System.out.println("Laptop object created");
-    }
-
     public void compile() {
         System.out.println("Compiling from Laptop...");
     }
