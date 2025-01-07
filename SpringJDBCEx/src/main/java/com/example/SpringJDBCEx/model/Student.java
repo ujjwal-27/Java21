9675875dead7d2103eb9be171be62c70d3fffe8b
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-07 19:44:44 +0545

    create bean:Student

---
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/model/Student.java
@@ -0,0 +1,34 @@
+package com.example.SpringJDBCEx.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+    private int marks;
+    private String name;
+    private int rollNo;
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+}
