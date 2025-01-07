9ea14c585e61c534d59add47a9d6d7c8811aa88a
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-07 19:54:35 +0545

    invoke bean:StudentService and addStudent method

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/SpringJdbcExApplication.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/SpringJdbcExApplication.java
@@ -1,6 +1,7 @@
 package com.example.SpringJDBCEx;
 
 import com.example.SpringJDBCEx.model.Student;
+import com.example.SpringJDBCEx.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +16,9 @@ public class SpringJdbcExApplication {
 		student.setMarks(77);
 		student.setName("Ujjwal");
 		student.setRollNo(27);
+
+		StudentService studentService = context.getBean(StudentService.class);
+		studentService.addStudent(student);
 	}
 
 }
