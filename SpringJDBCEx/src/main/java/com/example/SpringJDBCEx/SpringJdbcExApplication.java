5960c1c850f34b9dc734a53a22d5c2aee0fd2d78
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-07 18:13:16 +0545

    initial commit

---
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/SpringJdbcExApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringJDBCEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcExApplication.class, args);
+	}
+
+}
