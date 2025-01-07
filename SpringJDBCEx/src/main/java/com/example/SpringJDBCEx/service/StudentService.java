7b232b3c746592a300637f859a4210435c000ee9
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-07 19:51:12 +0545

    create addStudent method in service

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/service/StudentService.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/service/StudentService.java
@@ -1,7 +1,11 @@
 package com.example.SpringJDBCEx.service;
 
+import com.example.SpringJDBCEx.model.Student;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+    public void addStudent(Student student) {
+        System.out.println("Student " + student.getName() + " added.");
+    }
 }
