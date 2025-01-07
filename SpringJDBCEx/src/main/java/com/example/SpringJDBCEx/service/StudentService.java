88d96167237ac37f8d288f34d0dfb20d0a6ac22b
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-07 19:59:17 +0545

    create setter-getter for student respository

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/service/StudentService.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/service/StudentService.java
@@ -1,10 +1,23 @@
 package com.example.SpringJDBCEx.service;
 
+import com.example.SpringJDBCEx.Repository.StudentRepository;
 import com.example.SpringJDBCEx.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+    private StudentRepository studentRepository;
+
+    public StudentRepository getStudentRepository() {
+        return studentRepository;
+    }
+
+    @Autowired
+    public void setStudentRepository(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public void addStudent(Student student) {
         System.out.println("Student " + student.getName() + " added.");
     }
