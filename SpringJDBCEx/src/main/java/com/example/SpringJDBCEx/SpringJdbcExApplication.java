e1b19a5fd60d34c38e032853df1a9ef96ad290ba
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-07 19:47:22 +0545

    invoke bean:Student and set values

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/SpringJdbcExApplication.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/SpringJdbcExApplication.java
@@ -1,13 +1,20 @@
 package com.example.SpringJDBCEx;
 
+import com.example.SpringJDBCEx.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcExApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringJdbcExApplication.class, args);
+
+		Student student = context.getBean(Student.class);
+		student.setMarks(77);
+		student.setName("Ujjwal");
+		student.setRollNo(27);
 	}
 
 }
