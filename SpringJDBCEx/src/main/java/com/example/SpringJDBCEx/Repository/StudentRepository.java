76326bef133ac95e8cc9eb59ec0d49c6a808adba
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-08 19:11:15 +0545

    create field, getters and setters for jdbc template in repository

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/Repository/StudentRepository.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/Repository/StudentRepository.java
@@ -1,11 +1,24 @@
 package com.example.SpringJDBCEx.Repository;
 
 import com.example.SpringJDBCEx.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentRepository {
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
     public void save(Student student) {
         System.out.println("Student " + student.getName() + " added.");
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
     }
 }
