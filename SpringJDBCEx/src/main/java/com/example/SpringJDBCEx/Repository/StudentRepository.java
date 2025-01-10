b0fbbacf4a761be50c7db0ed2d85b5fa08d96401
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-11 01:55:32 +0545

    implement findAll method to fetch list of students

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/Repository/StudentRepository.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/Repository/StudentRepository.java
@@ -3,7 +3,12 @@ package com.example.SpringJDBCEx.Repository;
 import com.example.SpringJDBCEx.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class StudentRepository {
@@ -19,8 +24,31 @@ public class StudentRepository {
         System.out.println(rows + " affected."); // since, update() method returns no. of rows affected by above query
     }
 
+    /**
+     * Spring will be responsible to instantiate the object. Everything will be handled by spring behind the scene.
+     * @param jdbc JDBC Template
+     */
     @Autowired
     public void setJdbc(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
+    }
+
+    public List<Student> findAll() {
+        String sql = "Select * from student";
+
+        RowMapper<Student> rowMapper = new RowMapper<Student>() {
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Student student = new Student();
+
+                student.setRollNo(rs.getInt("roll_no"));
+                student.setName(rs.getString("name"));
+                student.setMarks(rs.getInt("marks"));
+
+                return student;
+            }
+        };
+
+        return jdbc.query(sql, rowMapper);
     }
 }
