63a1ce37ce2918d465e94f2cbb554f08a2275cbb
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-11 16:52:11 +0545

    remove unused imports

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/Repository/StudentRepository.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/Repository/StudentRepository.java
@@ -3,11 +3,8 @@ package com.example.SpringJDBCEx.Repository;
 import com.example.SpringJDBCEx.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
