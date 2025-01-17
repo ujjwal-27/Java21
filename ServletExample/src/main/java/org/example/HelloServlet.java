a37209d805d4564b941745fca0090eb665e8ff4f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-17 21:58:18 +0545

    display message in client

--- ServletExample/src/main/java/org/example/HelloServlet.java
+++ ServletExample/src/main/java/org/example/HelloServlet.java
@@ -5,10 +5,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println("In service");
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+
+        out.println("<h2>Hello World!</h2>");
     }
 }
