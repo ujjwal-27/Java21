2f4806628171786756ebada3960f0aaf3f35056f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-17 17:03:48 +0545

    override service method

--- ServletExample/src/main/java/org/example/HelloServlet.java
+++ ServletExample/src/main/java/org/example/HelloServlet.java
@@ -1,6 +1,14 @@
 package org.example;
 
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("In service");
+    }
 }
