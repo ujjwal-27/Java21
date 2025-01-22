ec8112e2feee0c3ae519505a43caafb32c37bde3
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 17:14:20 +0545

    test controller

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class HomeController {
     public String home() {
+        System.out.println("Home controller called");
         return "index.jsp";
     }
 }
