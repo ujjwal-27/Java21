0bcccad897c20db53c30fc227262ebb3825b9fee
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 17:32:50 +0545

    define url in RequestMapping

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping({"/","/home"})
     public String home() {
         System.out.println("Home controller called");
         return "index.jsp";
