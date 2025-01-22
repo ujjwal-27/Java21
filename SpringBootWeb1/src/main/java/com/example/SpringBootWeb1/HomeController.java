f633de21d737144e806fe3230c291ced8605bbb6
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 17:25:35 +0545

    add RequestMapping annotation

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -1,9 +1,12 @@
 package com.example.SpringBootWeb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping("/")
     public String home() {
         System.out.println("Home controller called");
         return "index.jsp";
