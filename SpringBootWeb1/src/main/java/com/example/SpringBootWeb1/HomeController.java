e13fdff7d45acea8bf2c428a936fc92b44f162f6
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-22 17:36:47 +0545

    add comments

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -3,9 +3,18 @@ package com.example.SpringBootWeb1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * In spring boot, always use @Controller annotation for a controller class
+ */
 @Controller
 public class HomeController {
 
+    /**
+     * @RequestMapping This annotation is used to map 'url' with this method -> home, which loads view page.
+     * The url is defined in the param of annotation.
+     * Multiple url can be defined using array.
+     * @return View page -> index.jsp
+     */
     @RequestMapping({"/","/home"})
     public String home() {
         System.out.println("Home controller called");
