7f22cdd7ae04665d5541f60202f494dc92946a35
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-26 22:55:40 +0545

    add programmingLanguage method (understanding ModelAttibute)

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/ProgrammerController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/ProgrammerController.java
@@ -1,11 +1,17 @@
 package com.example.SpringBootWeb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProgrammerController {
+
+    @ModelAttribute("language")
+    public String programmingLanguage() {
+        return "Java";
+    }
 
     @RequestMapping("/programmerForm")
     public String programmerForm() {
