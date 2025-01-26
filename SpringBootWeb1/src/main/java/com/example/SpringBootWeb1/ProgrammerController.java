d949c520be849ee50ca6b442f36873d345d7bbf1
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-26 22:51:30 +0545

    update addProgrammer method, and add comments

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/ProgrammerController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/ProgrammerController.java
@@ -14,15 +14,29 @@ public class ProgrammerController {
         return mv;
     }
 
+    /**
+     * In this method, instead of accepting data from form through multiple parameters, we are accepting whole programmer model in the form of data.
+     * Note that, for this to work, the name of form field must match with the properties of programmer model.
+     * This is an example of using ModelAttribute.
+     * The parameter can also be written as: (@ModelAttribute("data") Programmer programmer). This is optional, and not really required until we need to change the name of data we are sending.
+     * If the attributeName is set, then that attributeName must be used in view page.
+     * @param programmer [Programmer] An object of programmer
+     * @return [String] View page: programmerResult
+     */
     @RequestMapping("/addProgrammer")
-    public ModelAndView addProgrammer(int pid, String name, ModelAndView mv) {
-        Programmer programmer = new Programmer();
-        programmer.setProgrammerId(pid);
-        programmer.setProgrammerName(name);
-
-        mv.addObject("programmer", programmer);
-        mv.setViewName("programmerResult");
-
-        return mv;
+    public String addProgrammer(Programmer programmer) {
+        return "programmerResult";
     }
+
+//    @RequestMapping("/addProgrammer")
+//    public ModelAndView addProgrammer(int programmerId, String programmerName, ModelAndView mv) {
+//        Programmer programmer = new Programmer();
+//        programmer.setProgrammerId(programmerId);
+//        programmer.setProgrammerName(programmerName);
+//
+//        mv.addObject("programmer", programmer);
+//        mv.setViewName("programmerResult");
+//
+//        return mv;
+//    }
 }
