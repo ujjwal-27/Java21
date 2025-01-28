e3ed40e9324808f4f55109b8d7cce8bbc49a2a34
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-28 22:12:07 +0545

    add programmer model and controller

---
+++ SpringMVCDemo/src/main/java/com/example/SpringMVCDemo/Programmer.java
@@ -0,0 +1,30 @@
+package com.example.SpringBootWeb1;
+
+public class Programmer {
+    private int programmerId;
+    private String programmerName;
+
+    public int getProgrammerId() {
+        return programmerId;
+    }
+
+    public String getProgrammerName() {
+        return programmerName;
+    }
+
+    public void setProgrammerId(int programmerId) {
+        this.programmerId = programmerId;
+    }
+
+    public void setProgrammerName(String programmerName) {
+        this.programmerName = programmerName;
+    }
+
+    @Override
+    public String toString() {
+        return "Programmer{" +
+                "programmerId=" + programmerId +
+                ", programmerName='" + programmerName + '\'' +
+                '}';
+    }
+}
