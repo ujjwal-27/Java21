e00ab143e475feaa38727f56e7c7b45cfad77e1f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-03 17:04:39 +0545

    implement RequestMapping for home page

---
+++ JobApp/src/main/java/com/example/JobApp/JobController.java
@@ -0,0 +1,13 @@
+package com.example.JobApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/", "home"})
+    public String home() {
+        return "home";
+    }
+}
