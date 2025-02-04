9673cc6bf16ae7e45b6fd95868173e10c1560d79
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-04 17:49:19 +0545

    update mapping annotations

--- JobApp/src/main/java/com/example/JobApp/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/JobController.java
@@ -1,18 +1,24 @@
 package com.example.JobApp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobController {
 
-    @RequestMapping({"/", "home"})
+    @GetMapping({"/", "home"})
     public String home() {
         return "home";
     }
 
-    @RequestMapping("addjob")
+    @GetMapping("addjob")
     public String addJob() {
         return "addjob";
+    }
+
+    @PostMapping("handleForm")
+    public String handleForm() {
+        return "success";
     }
 }
