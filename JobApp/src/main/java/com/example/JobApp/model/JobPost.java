a8e20c70a9df79bc3c0073a9b0cbbfc76ca42376
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-06 18:58:12 +0545

    create JobPost model

--- JobApp/src/main/java/com/example/JobApp/model/JobPost.java
+++ JobApp/src/main/java/com/example/JobApp/model/JobPost.java
@@ -7,17 +7,9 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
-/**
- * Lombok is a Java Library that helps reduce the boilerplate code by automatically generating common methods required in a model class.
- * It implements several annotation for it.
- * @Data - Automatically generates methods like getter, setter, toString, equals, and hashCode.
- * @AllArgsConstructor - Automatically generates parameterized constructor with all the class properties defined in its parameter.
- * @NoArgsConstructor - Automatically generates default constructor.
- */
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Component
 public class JobPost {
     private int postId;
