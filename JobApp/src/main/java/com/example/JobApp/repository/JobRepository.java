c14bb9e6579a59a8cffddd10816462b51d33ea72
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-10 20:02:29 +0545

    use Repository annotation

--- JobApp/src/main/java/com/example/JobApp/repository/JobRepository.java
+++ JobApp/src/main/java/com/example/JobApp/repository/JobRepository.java
@@ -1,11 +1,13 @@
 package com.example.JobApp.repository;
 
 import com.example.JobApp.model.JobPost;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Repository
 public class JobRepository {
     List<JobPost> jobs = new ArrayList<>(Arrays.asList(
             new JobPost(1, "Java Developer", "Must have good experience in core Java and advanced Java", 2, List.of("Core Java", "J2EE", "Spring Boot", "Hibernate")),
@@ -21,5 +23,6 @@ public class JobRepository {
 
     public void addJob(JobPost job) {
         jobs.add(job);
+        System.out.println(jobs);
     }
 }
