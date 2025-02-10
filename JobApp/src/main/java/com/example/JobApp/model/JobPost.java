9a8d1056a955492819d63c091b3a6994af1bbd89
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-10 17:46:29 +0545

    create getters and setters (lombok not working)

--- JobApp/src/main/java/com/example/JobApp/model/JobPost.java
+++ JobApp/src/main/java/com/example/JobApp/model/JobPost.java
@@ -17,4 +17,44 @@ public class JobPost {
     private String postDesc;
     private int reqExperience;
     private List<String> postTechStack;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getPostProfile() {
+        return postProfile;
+    }
+
+    public void setPostProfile(String postProfile) {
+        this.postProfile = postProfile;
+    }
+
+    public String getPostDesc() {
+        return postDesc;
+    }
+
+    public void setPostDesc(String postDesc) {
+        this.postDesc = postDesc;
+    }
+
+    public int getReqExperience() {
+        return reqExperience;
+    }
+
+    public void setReqExperience(int reqExperience) {
+        this.reqExperience = reqExperience;
+    }
+
+    public List<String> getPostTechStack() {
+        return postTechStack;
+    }
+
+    public void setPostTechStack(List<String> postTechStack) {
+        this.postTechStack = postTechStack;
+    }
 }
