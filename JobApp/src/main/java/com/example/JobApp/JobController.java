e4bc200d1543210db1dd4b3462d18edcfa3b351a
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-10 19:52:57 +0545

    autowire JobService and send jobPost data

--- JobApp/src/main/java/com/example/JobApp/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/JobController.java
@@ -1,12 +1,16 @@
 package com.example.JobApp;
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobController {
+    @Autowired
+    private JobService service;
 
     @GetMapping({"/", "home"})
     public String home() {
@@ -20,6 +24,7 @@ public class JobController {
 
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost) {
+        service.addJob(jobPost);
         return "success";
     }
 }
