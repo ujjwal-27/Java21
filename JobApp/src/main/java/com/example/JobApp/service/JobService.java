d6a8dfdbadb415b7d39e66dc94fcad2b3f8f04a9
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-10 19:56:26 +0545

    autowire JobRepository, send jobPost data, and get all jobs

--- JobApp/src/main/java/com/example/JobApp/service/JobService.java
+++ JobApp/src/main/java/com/example/JobApp/service/JobService.java
@@ -1,15 +1,22 @@
 package com.example.JobApp.service;
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class JobService {
-    public void addJob() {
+    @Autowired
+    private JobRepository repository;
 
+    public void addJob(JobPost jobPost) {
+        repository.addJob(jobPost);
     }
 
     public List<JobPost> getAllJobs() {
-        
+        return repository.getAllJobs();
     }
 }
