3027b0204423c933f5fe7a24d665602538be5070
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-10 17:50:00 +0545

    create service layer

---
+++ JobApp/src/main/java/com/example/JobApp/service/JobService.java
@@ -0,0 +1,15 @@
+package com.example.JobApp.service;
+
+import com.example.JobApp.model.JobPost;
+
+import java.util.List;
+
+public class JobService {
+    public void addJob() {
+
+    }
+
+    public List<JobPost> getAllJobs() {
+        
+    }
+}
