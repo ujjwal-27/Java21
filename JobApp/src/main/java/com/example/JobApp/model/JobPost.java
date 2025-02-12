1a42cb9332f51780edd2c9668c5d2629ccff50fd
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-12 17:05:17 +0545

    create parameterized constructor explicitly

--- JobApp/src/main/java/com/example/JobApp/model/JobPost.java
+++ JobApp/src/main/java/com/example/JobApp/model/JobPost.java
@@ -1,6 +1,5 @@
 package com.example.JobApp.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,7 +8,6 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Component
 public class JobPost {
     private int postId;
@@ -17,4 +15,12 @@ public class JobPost {
     private String postDesc;
     private int reqExperience;
     private List<String> postTechStack;
+
+    public JobPost(int postId, String postProfile, String postDesc, int reqExperience, List<String> postTechStack) {
+        this.postId = postId;
+        this.postProfile = postProfile;
+        this.postDesc = postDesc;
+        this.reqExperience = reqExperience;
+        this.postTechStack = postTechStack;
+    }
 }
