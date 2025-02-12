f86f95cf96ff1150542994c6eb91f365c48e200d
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-02-12 16:59:03 +0545

    autowire JobService, and transfer jobPost object to service (DTO)

--- JobApp/src/main/java/com/example/JobApp/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/JobController.java
@@ -1,6 +1,8 @@
 package com.example.JobApp;
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobController {
+    @Autowired
+    private JobService jobService;
+
     @GetMapping({"/", "/home"})
     public String home() {
         return "home";
@@ -22,6 +27,7 @@ public class JobController {
     @PostMapping("handleForm")
     public String handleForm(@ModelAttribute JobPost jobPost, Model model) {
         model.addAttribute("jobPost", jobPost);
+        jobService.addJob(jobPost);
         return "success";
     }
 }
