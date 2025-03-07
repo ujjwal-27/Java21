7ed7a8ce65cc30c5484ca02c19e0ea0f9fb82eb3
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-07 19:14:15 +0545

    implement findById

--- spring-data-jpa-example/src/main/java/com/example/spring_data_jpa_example/SpringDataJpaExampleApplication.java
+++ spring-data-jpa-example/src/main/java/com/example/spring_data_jpa_example/SpringDataJpaExampleApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class SpringDataJpaExampleApplication {
 	/**
@@ -19,25 +21,28 @@ public class SpringDataJpaExampleApplication {
 
 		Student s1 = context.getBean(Student.class);
 		Student s2 = context.getBean(Student.class);
-		Student s3 = context.getBean(Student.class);;
+		Student s3 = context.getBean(Student.class);
 
-		s1.setRollNo(101);
-		s1.setName("Ujjwal");
-		s1.setMarks(99);
+//		s1.setRollNo(101);
+//		s1.setName("Ujjwal");
+//		s1.setMarks(99);
+//
+//		s2.setRollNo(102);
+//		s2.setName("Jason");
+//		s2.setMarks(87);
+//
+//		s3.setRollNo(103);
+//		s3.setName("Jane");
+//		s3.setMarks(75);
+//
+//		studentRepository.save(s1);
+//		studentRepository.save(s2);
+//		studentRepository.save(s3);
+//
+//		System.out.println(studentRepository.findAll());
 
-		s2.setRollNo(102);
-		s2.setName("Jason");
-		s2.setMarks(87);
-
-		s3.setRollNo(103);
-		s3.setName("Jane");
-		s3.setMarks(75);
-
-		studentRepository.save(s1);
-		studentRepository.save(s2);
-		studentRepository.save(s3);
-
-		System.out.println(studentRepository.findAll());
+		Optional<Student> student = studentRepository.findById(101); // findById() method returns Optional<T> type. Optional basically handles null value.
+		System.out.println(student.orElse(new Student())); // If the data is found, it will return the Student, else it will return empty Student object.
 	}
 
 }
