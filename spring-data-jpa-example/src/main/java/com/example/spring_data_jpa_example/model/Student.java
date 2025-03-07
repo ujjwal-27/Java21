701f76ef5c942e384e0056b01c398d1a70f32756
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-07 14:40:02 +0545

    create student model class

---
+++ spring-data-jpa-example/src/main/java/com/example/spring_data_jpa_example/model/Student.java
@@ -0,0 +1,45 @@
+package com.example.spring_data_jpa_example.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Student {
+    private int marks;
+    private String name;
+    private int roll_no;
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNo() {
+        return roll_no;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.roll_no = rollNo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "marks=" + marks +
+                ", name='" + name + '\'' +
+                ", roll_no=" + roll_no +
+                '}';
+    }
+}
