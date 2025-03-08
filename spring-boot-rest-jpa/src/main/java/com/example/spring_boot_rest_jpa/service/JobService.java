7cdf40d339aa85e3c59ab243c72c7afe8f5a1795
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-08 15:52:23 +0545

    add model class, controller, service and repository

---
+++ spring-boot-rest-jpa/src/main/java/com/example/spring_boot_rest_jpa/service/JobService.java
@@ -0,0 +1,34 @@
+package com.example.spring_boot_rest_jpa.service;
+
+import com.example.spring_boot_rest_jpa.model.JobPost;
+import com.example.spring_boot_rest_jpa.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+    @Autowired
+    private JobRepository jobRepository;
+
+    public void addJob(JobPost jobPost) {
+        jobRepository.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs() {
+        return jobRepository.getAllJobs();
+    }
+
+    public JobPost getJob(int postId) {
+        return jobRepository.getJob(postId);
+    }
+
+    public void updateJob(JobPost jobPost) {
+        jobRepository.updateJob(jobPost);
+    }
+
+    public void deleteJob(int postId) {
+        jobRepository.deleteJob(postId);
+    }
+}
