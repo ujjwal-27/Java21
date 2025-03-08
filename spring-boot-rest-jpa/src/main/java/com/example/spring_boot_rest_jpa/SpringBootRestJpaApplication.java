bbb109233854d294df3a6f161fc564ed37faca39
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-08 15:34:24 +0545

    initial commit

---
+++ spring-boot-rest-jpa/src/main/java/com/example/spring_boot_rest_jpa/SpringBootRestJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_rest_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestJpaApplication.class, args);
+	}
+
+}
---
+++ spring-boot-rest-jpa/src/test/java/com/example/spring_boot_rest_jpa/SpringBootRestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_rest_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
