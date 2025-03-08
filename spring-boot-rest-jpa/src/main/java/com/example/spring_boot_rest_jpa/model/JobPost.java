bcdfc18fb04bf4be6e80f04a97b4580dc44abaa2
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-08 19:04:48 +0545

    implement Entity and Id annotation

--- spring-boot-rest-jpa/src/main/java/com/example/spring_boot_rest_jpa/model/JobPost.java
+++ spring-boot-rest-jpa/src/main/java/com/example/spring_boot_rest_jpa/model/JobPost.java
@@ -1,5 +1,7 @@
 package com.example.spring_boot_rest_jpa.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
+@Entity
 public class JobPost {
+    @Id
     private int postId;
     private String postProfile;
     private String postDesc;
