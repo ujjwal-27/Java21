20b5f2aca6710ab9eedab9a5ec14d40d51e2e10e
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-11 13:47:39 +0545

    create product repository

---
+++ Section17/SpringEcom/src/main/java/com/example/SpringEcom/Repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.SpringEcom.Repository;
+
+import com.example.SpringEcom.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
