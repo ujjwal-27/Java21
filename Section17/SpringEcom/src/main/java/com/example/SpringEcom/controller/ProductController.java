fd6bc7e8583c37d236277c9c0ec06325c6c25811
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-11 13:48:10 +0545

    update product controller

--- Section17/SpringEcom/src/main/java/com/example/SpringEcom/controller/ProductController.java
+++ Section17/SpringEcom/src/main/java/com/example/SpringEcom/controller/ProductController.java
@@ -1,14 +1,24 @@
 package com.example.SpringEcom.controller;
 
+import com.example.SpringEcom.model.Product;
+import com.example.SpringEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("products")
-    public String getProducts() {
-        return "All products";
+    public List<Product> getProducts() {
+        return productService.getProducts();
     }
 }
