8ecd33720f892f0f0cd62bc4a5843be18351ecd3
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-12 14:36:51 +0545

    implement addProduct api endpoint

--- Section17/SpringEcom/src/main/java/com/example/SpringEcom/controller/ProductController.java
+++ Section17/SpringEcom/src/main/java/com/example/SpringEcom/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +40,18 @@ public class ProductController {
 
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        try {
+            Product savedProduct = productService.addProduct(product, image);
+
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
