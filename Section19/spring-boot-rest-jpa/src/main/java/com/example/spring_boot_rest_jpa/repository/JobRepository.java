429dc8d744a6169d846a46d8df9a35b9c70a8960
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-04-03 23:28:26 +0545

    initial commit for logging calls

---
+++ Section19/spring-boot-rest-jpa/src/main/java/com/example/spring_boot_rest_jpa/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_rest_jpa.repository;
+
+import com.example.spring_boot_rest_jpa.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepository extends JpaRepository<JobPost, Integer> {
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+}
