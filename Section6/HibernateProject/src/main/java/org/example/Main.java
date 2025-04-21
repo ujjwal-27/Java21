e86726cf36d7820045bd09e80c195b5b639bc33c
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-04-21 22:37:11 +0545

    configure hibernate session

--- Section6/HibernateProject/src/main/java/org/example/Main.java
+++ Section6/HibernateProject/src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student s1 = new Student();
+
+        s1.setAge(29);
+        s1.setName("Ujjwal");
+        s1.setRollNo(7);
+
+        System.out.println(s1);
+
+        Configuration config = new Configuration();
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        session.save(s1);
     }
 }
