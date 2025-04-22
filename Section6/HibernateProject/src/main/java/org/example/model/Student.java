64683a51e2d28516c082a0b5abf306710ff67843
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-04-22 15:25:26 +0545

    implement annotation:Id

--- Section6/HibernateProject/src/main/java/org/example/model/Student.java
+++ Section6/HibernateProject/src/main/java/org/example/model/Student.java
@@ -1,9 +1,11 @@
 package org.example.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Student {
+    @Id
     private int rollNo;
     private String name;
     private int age;
